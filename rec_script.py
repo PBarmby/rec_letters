e573f20498d307013e226b2a2bf96b736b43a360
Pauline Barmby <pbarmby@uwo.ca>
2015-11-28 13:37:23 -0500

    insert macro name into tex

--- rec_script.py
+++ rec_script.py
@@ -7,12 +7,15 @@ def main():
     f = open(address_list)
     for line in f.readlines():
         #error trap this line
-        id,ret_address,location,salutation,posname = string.split(line,";")
+        id,full_addressee,ret_address,location,salutation,posname = string.split(line,";")
+        # generate customized macro file for this address
         macro_file = "address_macro_%s" % id
-        generate_macro(macro_file, [ret_address,salutation,location,posname])
-#        texfile = main_letter_fname[:-4]+"_"+id+".tex"
+        generate_macro(macro_file, [full_addressee,ret_address,salutation,location,posname])
+
         # insert macro into texfile
-        # how to do? TBD
+        texfile = main_letter_fname[:-4]+"_"+id+".tex"
+        insert_macro(main_letter_fname,texfile, macro_file)
+
         # run latex
 #        os.system()
         # delete unneeded files
@@ -21,11 +24,23 @@ def main():
 
 def generate_macro(macro_file, content_list):
     outf = open(macro_file,"w")
-    command_list = ["\\newcommand{\\addressee}{%s}", "\\newcommand{\\dear}{%s}", "\\newcommand{\\yourplace}{%s}", "\\newcommand{\\jobname}{%s}"]
+    command_list = ["\\newcommand{\\addressee}{%s \\hfill\\today\\\\}", "\\newcommand{\\retaddress}{%s}", "\\newcommand{\\dear}{%s}", 
+                    "\\newcommand{\\yourplace}{%s}", "\\newcommand{\\posname}{%s}"]
 
     for cmnd, cnt in zip(command_list,content_list):
          outstr = cmnd  % cnt
          outf.write(outstr+"\n")
+    outf.close()
+    return
+
+def insert_macro(orig_file, new_file, macro_name):
+    inf = open(orig_file,"r")
+    outf = open(new_file, "w")
+    for line in inf.readlines():
+        if "address_macro" in line:
+            line = "\\input{%s}" % macro_name
+        outf.write(line)
+    inf.close()
     outf.close()
     return
 
